17986731d4da7c75174323a32a5d4e9fe17bd4ed
Emami.114 <emami.114@outlook.de>
2023-05-17 09:20:24 +0200

    Update romanToInt

--- src/main/kotlin/RomanToInt.kt
+++ src/main/kotlin/RomanToInt.kt
@@ -4,6 +4,7 @@
 
 fun main(){
     println(romanToInt("XII"))
+    println(RomanToInt("XII"))
 }
 
 /**For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
@@ -14,6 +15,38 @@ I can be placed before V (5) and X (10) to make 4 and 9.
 X can be placed before L (50) and C (100) to make 40 and 90.
 C can be placed before D (500) and M (1000) to make 400 and 900.
 Given a roman numeral, convert it to an integer.*/
+
+fun RomanToInt(roman: String): Int {
+    val values = mapOf(
+        'I' to 1,
+        'V' to 5,
+        'X' to 10,
+        'L' to 50,
+        'C' to 100,
+        'D' to 500,
+        'M' to 1000
+    )
+
+    var result = 0
+    var previousValue = 0
+
+    for (i in roman.indices.reversed()) {
+        val currentValue = values[roman[i]] ?: 0
+
+        if (currentValue >= previousValue) {
+            result += currentValue
+        } else {
+            result -= currentValue
+        }
+
+        previousValue = currentValue
+    }
+
+    return result
+}
+
+
+
 fun romanToInt(s: String): Int {
     val map = hashMapOf('I' to 1, 'V' to 5, 'X' to 10, 'L' to 50, 'C' to 100, 'D' to 500, 'M' to 1000)
     var result = 0
