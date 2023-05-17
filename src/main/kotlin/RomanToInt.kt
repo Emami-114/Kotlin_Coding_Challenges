6ef3612a8d803cf6b37e930dc86ba6867f3c1395
Emami.114 <emami.114@outlook.de>
2023-05-17 09:22:15 +0200

    Update romanToInt

--- src/main/kotlin/RomanToInt.kt
+++ src/main/kotlin/RomanToInt.kt
@@ -7,6 +7,34 @@ fun main(){
     println(RomanToInt("XII"))
 }
 
+/**
+ * ----Detsch---
+ *
+ * Schreibe in der Main.kt die Funktion `RomanToInt()`, die eine römische Zahl als Parameter annimmt und ihren Wert als Integer zurückgibt.
+ *
+ * Moderne römische Ziffern werden geschrieben, indem jede Dezimalziffer der dargestellten Zahl separat ausgedrückt wird, beginnend mit der äußersten linken Ziffer und unter Auslassung aller Nullen.
+ * So wird 1990 als *"MCMXC"* (1000 = M, 900 = CM, 90 = XC) und 2008 als *"MMVIII"* (2000 = MM, 8 = VIII) wiedergegeben.
+ * Die römische Zahl für 1666, *"MDCLXVI"*, verwendet jeden Buchstaben in absteigender Reihenfolge.
+ *
+ * *Beispiel:*
+ *
+ * - *RomanToInt("XXI") // sollte 21 zurückgeben*
+ *
+ * Hilfe:
+ *
+ * Symbol Wert
+ * - I = 1
+ * - V = 5
+ * - X = 10
+ * - L = 50
+ * - C = 100
+ * - D = 500
+ * - M = 1,000
+ *
+ * */
+
+
+
 /**For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
 Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
